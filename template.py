5908afde4a6ba8f0dc18192f1ab00ba6ce6ef059
Peter (Rocky) Villano <peter.s.villano@gmail.com>
2021-12-11 13:56:44 -0500

    created template

---
+++ template.py
@@ -0,0 +1,19 @@
+from utils import *
+from math import *
+from itertools import *
+
+test = """"""
+
+lines = get_day(TODO, test).split('\n')
+
+
+def part1():
+    pass
+
+
+def part2():
+    pass
+
+
+part1()
+part2()
