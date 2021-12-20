d674863aaa9a858c774bec167c51e0eb9aeaae88
Peter (Rocky) Villano <peter.s.villano@gmail.com>
2021-12-19 20:45:49 -0500

    redid part 1

--- day16.py
+++ day16.py
@@ -1,83 +1,67 @@
 import operator
 from functools import reduce
 
-from utils import benchmark, debug_print, get_day
+from utils import benchmark, debug_print, get_day, debug_print_recursive
 
-test = """9C0141080250320F1802104A08"""
+test = """A0016C880162017C3686B18A3D4780"""
 
 line = get_day(16, test, override=True)
-# data = (tuple(map(int, line)) for line in lines)
-# data = (tuple(map(int, line.split('\n'))) for line in lines)
-data = int(line, 16)
-data = format(data, "b").rjust(len(line) * 4, "0")
-# data = data.rstrip('0')
-data = tuple(int(x) for x in data)
-# debug_print(data)
-data: tuple[int]
+data_long = int(line, 16)
+data_str = format(data_long, "b").rjust(len(line) * 4, "0")
+data: tuple[int] = tuple(int(x) for x in data_str)
 
 
-def list_int(l):
-    return int("".join(map(str, l)), 2)
+def list_to_int(l):
+    as_str = "".join(str(x) for x in l)
+    return int(as_str, 2)
+
+
+def decode_num(remaining):
+    num = 0
+    while remaining[0] == 1:
+        _, *int_bits = remaining[:5]
+        remaining = remaining[5:]
+        num = (num << 4) + list_to_int(int_bits)
+    _, *int_bits = remaining[:5]
+    remaining = remaining[5:]
+    num = (num << 4) + list_to_int(int_bits)
+    debug_print(f"{num=}")
+    return num, remaining
+
+
+def pluck_a_packet(remaining):
+    debug_print_recursive("before", "".join(map(str, remaining)))
+    version, type_id = list_to_int(remaining[0:3]), list_to_int(remaining[3:6])
+    remaining = remaining[6:]
+    debug_print_recursive(f"{version=} {type_id=}", end=" ")
+    if type_id == 4:
+        num, remaining = decode_num(remaining)
+        # return num, remaining
+        return version, remaining
+    else:
+        length_type_id, *remaining = remaining
+        sub_packets = []
+        if length_type_id == 0:
+            len_sub_packets, remaining = list_to_int(remaining[:15]), remaining[15:]
+            debug_print(f"{length_type_id=} {len_sub_packets=}")
+            target_length = len(remaining) - len_sub_packets
+            while len(remaining) > target_length:
+                packet, remaining = pluck_a_packet(remaining)
+                sub_packets.append(packet)
+            if len(remaining) < target_length:
+                raise Exception()
+        else:
+            num_sub_packets, remaining = list_to_int(remaining[:11]), remaining[11:]
+            debug_print(f"{length_type_id=} {num_sub_packets=}")
+            for i in range(num_sub_packets):
+                packet, remaining = pluck_a_packet(remaining)
+                sub_packets.append(packet)
+    debug_print_recursive("after", "".join(map(str, remaining)))
+    return version + sum(sub_packets), remaining
 
 
 def part1():
-    return parse1(data)
-
-
-def parse1(remaining, lennn=1):
-    ret = []
-    for i in range(lennn):
-        if not remaining:
-            break
-        ver, id, remaining = remaining[:3], remaining[3:6], remaining[6:]
-        debug_print(f"{ver=}")
-        if id == (1, 0, 0):  # literal
-            debug_print("literal")
-            litval = 0
-            bits, remaining = remaining[:5], remaining[5:]
-            debug_print(bits)
-            while bits[0] == 1:
-                debug_print(bits)
-                litval <<= 4
-                litval += list_int(bits[1:])
-                bits, remaining = remaining[:5], remaining[5:]
-            debug_print(bits)
-            litval <<= 4
-            litval += list_int(bits[1:])
-            debug_print(litval)
-            ret.append(litval)
-        else:  # operator
-            opnum = list_int(id)
-            debug_print("op", opnum)
-            operators = (
-                operator.add,
-                operator.mul,
-                min,
-                max,
-                "literal",
-                operator.gt,
-                operator.lt,
-                operator.eq,
-            )
-            length_type, remaining = remaining[0], remaining[1:]
-            if length_type == 0:
-                total_length_bits, remaining = remaining[:15], remaining[15:]
-                lenpackets = list_int(total_length_bits)
-                debug_print("substing length", lenpackets)
-                sub_packet, remaining = remaining[:lenpackets], remaining[lenpackets:]
-                ret.append(
-                    reduce(
-                        operators[opnum], parse1(sub_packet, 9999999999999999999999)[0]
-                    )
-                )
-            else:
-                numpacketsbits, remaining = remaining[:11], remaining[11:]
-                numpackets = list_int(numpacketsbits)
-                debug_print("count length", numpackets)
-
-                myval, remaining = parse1(remaining, numpackets)
-                ret.append(reduce(operators[opnum], myval))
-    return ret, remaining
+    return pluck_a_packet(list(data))[0]
 
 
 def part2():
