a482aa90fe9d52c0c2ececaea99a1bafd5f8dc50
Peter (Rocky) Villano <peter.s.villano@gmail.com>
2021-12-19 20:55:21 -0500

    did day 16 part 2

--- day16.py
+++ day16.py
@@ -3,9 +3,9 @@ from functools import reduce
 
 from utils import benchmark, debug_print, get_day, debug_print_recursive
 
-test = """A0016C880162017C3686B18A3D4780"""
+test = """9C0141080250320F1802104A08"""
 
-line = get_day(16, test, override=True)
+line = get_day(16, test)
 data_long = int(line, 16)
 data_str = format(data_long, "b").rjust(len(line) * 4, "0")
 data: tuple[int] = tuple(int(x) for x in data_str)
@@ -64,10 +64,80 @@ def part1():
     return pluck_a_packet(list(data))[0]
 
 
+def prod(items):
+    running = 1
+    for item in items:
+        running *= item
+    return running
+
+
+def gt(items):
+    assert len(items) == 2
+    first, second = items
+    if first > second:
+        return 1
+    return 0
+
+
+def lt(items):
+    return gt(items[::-1])
+
+
+def eq(items):
+    assert len(items) == 2
+    first, second = items
+    if first == second:
+        return 1
+    return 0
+
+
+function_map = {
+    0: sum,
+    1: prod,
+    2: min,
+    3: max,
+    5: gt,
+    6: lt,
+    7: eq,
+}
+
+
+def pluck_a_packet2(remaining):
+    debug_print_recursive("before", "".join(map(str, remaining)))
+    version, type_id = list_to_int(remaining[0:3]), list_to_int(remaining[3:6])
+    remaining = remaining[6:]
+    debug_print_recursive(f"{version=} {type_id=}", end=" ")
+    if type_id == 4:
+        num, remaining = decode_num(remaining)
+        debug_print_recursive("after", "".join(map(str, remaining)))
+        return num, remaining
+    # else
+    length_type_id, *remaining = remaining
+    sub_packets = []
+    if length_type_id == 0:
+        len_sub_packets, remaining = list_to_int(remaining[:15]), remaining[15:]
+        debug_print(f"{length_type_id=} {len_sub_packets=}")
+        target_length = len(remaining) - len_sub_packets
+        while len(remaining) > target_length:
+            packet, remaining = pluck_a_packet2(remaining)
+            sub_packets.append(packet)
+        if len(remaining) < target_length:
+            raise Exception()
+    else:
+        num_sub_packets, remaining = list_to_int(remaining[:11]), remaining[11:]
+        debug_print(f"{length_type_id=} {num_sub_packets=}")
+        for i in range(num_sub_packets):
+            packet, remaining = pluck_a_packet2(remaining)
+            sub_packets.append(packet)
+    op = function_map[type_id]
+    debug_print_recursive("after", "".join(map(str, remaining)))
+    return op(sub_packets), remaining
+
+
 def part2():
-    pass
+    return pluck_a_packet2(data)[0]
 
 
 if __name__ == "__main__":
-    benchmark(part1)
+    # benchmark(part1)
     benchmark(part2)
